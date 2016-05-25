f93973c8a68b1809dff320df44aa7237756f8a8a
David PS <dps.helio@gmail.com>
2016-05-25 18:42:19 +0100

    where do the key goes?

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,8 +5,8 @@ set -e
 if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == 'master' ]]; then
     echo "Will push to ghpages"
     echo "pushing the run branch"
-    git push upstream run
+    git push origin run
     echo " pushing the website"
-    git push -f upstream gh-pages
+    git push -f origin gh-pages
 fi
 
