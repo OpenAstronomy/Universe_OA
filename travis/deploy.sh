5f0375e0f48f61658613503f4542c3183bb438f1
David PS <dps.helio@gmail.com>
2016-05-26 10:25:33 +0100

    Update the deployment script on the run branch

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,15 +1,11 @@
-
 #!/bin/bash
 set -e
 
 if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == 'master' ]]; then
     echo "Will push to ghpages"
-    chmod 600 $SSH_KEY
-    eval `ssh-agent -s`
-    ssh-add $SSH_KEY
     echo "pushing the run branch"
-    git push upstream run
+    git push origin run
     echo " pushing the website"
-    git push -f upstream gh-pages
+    git push -f origin gh-pages
 fi
 
