a91491d1b11107e0702954ceb363ed570d6f4194
David Pérez-Suárez <dps.helio@gmail.com>
2020-05-11 20:20:04 +0100

    Changes the sidebar into a top bar that shows checks for dates
    Instead of showing just red names if student hasn't post in the last two weeks,
    it also shows how well they are doing with a checkmark/cross for each time period.

--- plugins/sidebar/sidebar.py
+++ plugins/sidebar/sidebar.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 from nikola.plugin_categories import ConfigPlugin
 
 from collections import OrderedDict
@@ -22,10 +21,11 @@ class Sidebar(ConfigPlugin):
             team = post.tags[0]
             text = post.text()
             if author not in author_lists.keys():
-                author_lists[author] = {'date': datepost}
+                author_lists[author] = {'date': datepost, 'dates': [datepost]}
             else:
                 if datepost > author_lists[author]['date']:
                     author_lists[author] = {'date':{datepost}}
+                author_lists[author]['dates'].append(datepost)
             author_lists[author]['team'] = team
             post_link = post.meta['en']['link']
             if "medium" in post_link:
@@ -41,7 +41,21 @@ class Sidebar(ConfigPlugin):
 
         def html_bit(authors, site=None, context=None):
             utc = pytz.UTC
-            final_text = u'<div class="sidebar">'
+            text_mobile = """
+            <style>
+@media screen and (max-width: 600px) {
+.table-done {
+    visibility: hidden;
+    clear: both;
+    padding: 0px 300px;
+    width: 50%;
+    display: block;
+  }
+}
+            </style>
+            """
+
+            final_text = text_mobile + u'<div class="sidebar" style="width:100%; max-width:100%; float:initial; margin:0px;"><ul>'
             now_2w = datetime.datetime.utcnow() - datetime.timedelta(weeks=2)
             now_2w = now_2w.replace(tzinfo=utc)
             image = {'Astropy':'http://www.astropy.org/favicon.ico',
@@ -59,25 +73,50 @@ class Sidebar(ConfigPlugin):
                      'Sherpa': 'http://openastronomy.org/img/members/sherpa_logo.gif',
                      'TimeLab': 'http://openastronomy.org/img/members/timelab.png',
                      'CTLearn': 'https://github.com/ctlearn-project.png?size=40'}
+            date_ranges = [(datetime.datetime(2020,5,4).replace(tzinfo=utc), datetime.datetime(2020,6,1).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,6,1).replace(tzinfo=utc), datetime.datetime(2020,6,15).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,6,15).replace(tzinfo=utc), datetime.datetime(2020,6,29).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,6,29).replace(tzinfo=utc), datetime.datetime(2020,7,13).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,7,13).replace(tzinfo=utc), datetime.datetime(2020,7,27).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,7,27).replace(tzinfo=utc), datetime.datetime(2020,8,10).replace(tzinfo=utc)),
+                           (datetime.datetime(2020,8,10).replace(tzinfo=utc), datetime.datetime(2020,8,24).replace(tzinfo=utc)),
+            ]
             for author in authors.keys():
                 if authors[author]['date'].year < datetime.datetime.utcnow().year:
                     continue
                 if author == 'rkiman':
                     continue
                 style = ''
-                print(authors[author])
-                print(now_2w)
+                # filling table:
+                filltable = ""
+                for st, en in date_ranges:
+                    if datetime.datetime.utcnow().replace(tzinfo=utc) < en:
+                        symbol = "🔲"
+                        style = "opacity: 0.5;"
+                    else:
+                        anydate = any(filter(lambda x: st <= x <= en, authors[author]['dates']))
+                        if anydate:
+                            symbol = "✅"
+                        else:
+                            symbol = "❌"
+                    filltable += f"<span style=\"display:table-cell; width:7%;{style}\" data-toggle=\"tooltip\" title=\"{st:%Y-%m-%d} - {en:%Y-%m-%d}\">{symbol}</span>"
+
+                style = ''
                 if authors[author]['date'] < now_2w:
                     style = 'style="color:rgb(255,0,0)"'
-                final_text += u'''<li>
+                final_text += '''<li>
                    <img src="{img}" height="16" width="16">
                    <a href="{url}" data-toggle="tooltip" data-placement="top" title="{date}" {style}>
                     {author}
-                   </a>
-                </li>'''.format(img=image[authors[author]['team']], url=authors[author]['url'],
+                   </a> <span class="table-done" style="float: right; padding-right:600px;">{intable}</span>
+                </li>'''.format(img=image[authors[author]['team']],
+                                url=authors[author]['url'],
                                 date=authors[author]['date'],
-                                author=author, style=style)
-            final_text += u'</div>'
+                                author=author,
+                                style=style,
+                                intable=filltable,
+                )
+            final_text += '</ul></div>'
             return final_text
 
         site.template_hooks['page_header'].append(html_bit, True, author_lists)
